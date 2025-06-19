2aba1f276a9a028e8091269ea32c26a8a39e80b8
Votre nom <oce.rakotomalala@gmail.com>
2025-06-19 23:07:49 +0200

    Première v1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,8 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Dys et alrs",
+  description: "Dyslexique",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
